56184d0544f95f21f85018b8c3d5c3d82ffb07ce
igor <igor.pravdin163@gmail.com>
2022-09-08 22:03:24 +0400

    добавил задачу1

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число число: ");
+string x = Console.ReadLine()!;
+int lenx = x.Length;
+string x_reverse = string.Empty;
+
+for (int i = lenx-1; i>=0; i--)
+{
+x_reverse +=x[i];
+}
+if (x_reverse == x)
+{
+Console.WriteLine("ДА");
+}
+else Console.WriteLine("НЕТ");
+
+
