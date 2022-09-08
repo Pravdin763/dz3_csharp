a859ec24494aac3bd1c002714e5f9e0a24a8dc35
igor <igor.pravdin163@gmail.com>
2022-09-08 22:17:21 +0400

    задача 3

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+// Принимает число "н" и выдает таблицу кубов из чисел от 1 до "н"
+
+Console.WriteLine("Введите целое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Кубы чисел от 1 до {x}:");
+for (int i=1; i<=x; i++)
+{
+Console.Write($"{i*i*i}, ");
+}
